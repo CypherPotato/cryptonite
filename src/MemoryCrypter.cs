cc8073278511cf08060725970b5e4e3a96cfe7c0
CypherPotato <gab@proj.pw>
2023-11-25 15:33:08 -0300

    0.3

---
+++ src/MemoryCrypter.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Cryptography;
+
+namespace Cryptonite;
+
+public class MemoryCrypter : AbstractCrypter
+{
+    public MemoryCrypter(int nmSize, byte[] salt)
+    {
+        this.DeriveParameters.Salt = salt;
+        this.keyBytes = RandomNumberGenerator.GetBytes(nmSize);
+    }
+
+    public static MemoryCrypter FromPrivateKey(Span<byte> keyBytes, Span<byte> salt)
+    {
+        var mc = new MemoryCrypter(keyBytes.Length, salt.ToArray());
+        mc.keyBytes = keyBytes.ToArray();
+        return mc;
+    }
+}
